c002198284d629908d61baf7646e6c60a8aa3a5d
Roland Groza <roland.groza@epfl.ch>
2018-08-29 17:04:14 +0200

    chore: run build during release

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 set -e
 
-# 1. Update package version
+# 1. Check build
+yarn build
+
+# 2. Update package version
 # NOTE: We don't commit and tag since we want to create the tag/commit after we generated the changelog.
 # https://docs.npmjs.com/cli/version
 VERSION_NAME=$(npm --no-git-tag-version version $1)
 VERSION=${VERSION_NAME//[v]/}
 
-# 2. Generate changelog
+# 3. Generate changelog
 conventional-changelog -p angular -i CHANGELOG.md -s -r 0
 
-# 3. Commit & tag
-# 3.1 Commit package.json and CHANGELOG.md
-# 3.2 Create a tag with version
+# 4. Commit & tag
+# 4.1 Commit package.json and CHANGELOG.md
+# 4.2 Create a tag with version
 git add package.json CHANGELOG.md
 COMMIT_MESSAGE="Release ${VERSION}"
 git commit -m "${COMMIT_MESSAGE}"
@@ -20,3 +23,6 @@ git commit -m "${COMMIT_MESSAGE}"
 COMMIT_SHA=$(git rev-parse --verify HEAD)
 git tag -a ${VERSION_NAME} ${COMMIT_SHA} \
     -m "${COMMIT_MESSAGE}"
+
+# 5. Push
+git push --follow-tags
